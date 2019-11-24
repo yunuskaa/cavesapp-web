a38f28d46518f4998e3d32bb4550f8d6bc360777
Yunus Aka <akayunus12@gmail.com>
2019-11-24 11:35:14 +0300

    Video component

--- src/containers/landing/pages/Landing.jsx
+++ src/containers/landing/pages/Landing.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Partner from 'shared/assets/img/google.png';
+import Video from 'shared/assets/img/video-view.png';
+import PlayIcon from 'shared/assets/img/icon-white-play.svg';
 import LandingHero from '../shared/components/home/Hero';
 
 function Landing() {
@@ -9,10 +11,10 @@ function Landing() {
       <LandingHero />
 
       <div className="landing-partners">
-        <div className="col-lg-8 col-md-10 offset-lg-2 offset-md-1">
+        <div className="col-lg-8 col-md-10 offset-lg-2 offset-md-2">
           <div className="row">
-            {[0, 1, 2, 3].map(() => (
-              <div className="col-lg-3 col-md-6 col-12">
+            {[0, 1, 2, 3].map(i => (
+              <div className="col-lg-3 col-md-6 col-12" key={i}>
                 <div className="landing-partner">
                   <a href="https://www.google.com.tr/" target="_blank" rel="noopener noreferrer">
                     <img src={Partner} alt="Google" />
@@ -23,6 +25,19 @@ function Landing() {
           </div>
         </div>
       </div>
+      <div className="landing-video">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2">
+            <div className="landing-video-image">
+              <img src={Video} alt="landing-video" />
+            </div>
+            <div className="landing-video-button">
+              <img src={PlayIcon} alt="button" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1>Dom</h1>
     </div>
   );
 }
